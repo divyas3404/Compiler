b8a3e6b1a36a75dd9f4d393c5ce811e37bbcff39
unknown <divyas3404@gmail.com>
2023-03-10 18:34:39 +0530

    save all

---
+++ compiler/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace compiler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.compileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richtext = new System.Windows.Forms.RichTextBox();
+            this.richcompiler = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lines = new System.Windows.Forms.Label();
+            this.directory = new System.Windows.Forms.Label();
+            this.title = new System.Windows.Forms.Label();
+            this.filename = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem1,
+            this.saveToolStripMenuItem,
+            this.compileToolStripMenuItem,
+            this.runToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(947, 32);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(72, 28);
+            this.openToolStripMenuItem.Text = "Open ";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem1
+            // 
+            this.saveToolStripMenuItem1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
+            this.saveToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(60, 28);
+            this.saveToolStripMenuItem1.Text = "Save";
+            this.saveToolStripMenuItem1.Click += new System.EventHandler(this.saveToolStripMenuItem1_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(83, 28);
+            this.saveToolStripMenuItem.Text = "Save as";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // compileToolStripMenuItem
+            // 
+            this.compileToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.compileToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.compileToolStripMenuItem.Name = "compileToolStripMenuItem";
+            this.compileToolStripMenuItem.Size = new System.Drawing.Size(91, 28);
+            this.compileToolStripMenuItem.Text = "Compile";
+            this.compileToolStripMenuItem.Click += new System.EventHandler(this.compileToolStripMenuItem_Click);
+            // 
+            // runToolStripMenuItem
+            // 
+            this.runToolStripMenuItem.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.runToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(55, 28);
+            this.runToolStripMenuItem.Text = "Run";
+            this.runToolStripMenuItem.Click += new System.EventHandler(this.runToolStripMenuItem_Click);
+            // 
+            // richtext
+            // 
+            this.richtext.Location = new System.Drawing.Point(25, 61);
+            this.richtext.Name = "richtext";
+            this.richtext.Size = new System.Drawing.Size(892, 224);
+            this.richtext.TabIndex = 1;
+            this.richtext.Text = "";
+            this.toolTip1.SetToolTip(this.richtext, "Enter java program");
+            this.richtext.TextChanged += new System.EventHandler(this.richtext_TextChanged);
+            // 
+            // richcompiler
+            // 
+            this.richcompiler.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.richcompiler.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richcompiler.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.richcompiler.Location = new System.Drawing.Point(25, 304);
+            this.richcompiler.Name = "richcompiler";
+            this.richcompiler.Size = new System.Drawing.Size(892, 165);
+            this.richcompiler.TabIndex = 2;
+            this.richcompiler.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Lines :";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
+            this.panel1.Controls.Add(this.lines);
+            this.panel1.Controls.Add(this.directory);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(0, 491);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(947, 39);
+            this.panel1.TabIndex = 7;
+            // 
+            // lines
+            // 
+            this.lines.AutoSize = true;
+            this.lines.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lines.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lines.Location = new System.Drawing.Point(103, 7);
+            this.lines.Name = "lines";
+            this.lines.Size = new System.Drawing.Size(50, 24);
+            this.lines.TabIndex = 11;
+            this.lines.Text = "        ";
+            // 
+            // directory
+            // 
+            this.directory.AutoSize = true;
+            this.directory.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.directory.Location = new System.Drawing.Point(576, 8);
+            this.directory.Name = "directory";
+            this.directory.Size = new System.Drawing.Size(293, 24);
+            this.directory.TabIndex = 10;
+            this.directory.Text = "/                                                       ";
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(471, 41);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(0, 24);
+            this.title.TabIndex = 8;
+            // 
+            // filename
+            // 
+            this.filename.AutoSize = true;
+            this.filename.Font = new System.Drawing.Font("Microsoft Yi Baiti", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.filename.Location = new System.Drawing.Point(21, 38);
+            this.filename.Name = "filename";
+            this.filename.Size = new System.Drawing.Size(295, 20);
+            this.filename.TabIndex = 9;
+            this.filename.Text = "Filename                      ";
+            this.filename.Click += new System.EventHandler(this.filename_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(947, 542);
+            this.Controls.Add(this.filename);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.richcompiler);
+            this.Controls.Add(this.richtext);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem compileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richtext;
+        private System.Windows.Forms.RichTextBox richcompiler;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label filename;
+        private System.Windows.Forms.Label directory;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem1;
+        private System.Windows.Forms.Label lines;
+    }
+}
+
---
+++ compiler/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace compiler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Filter = "c | *.c";
+            DialogResult dr = op.ShowDialog();
+
+            if(dr==DialogResult.OK)
+            {
+                string path = op.FileName;
+                StreamReader sr = new StreamReader(path);
+                string data = sr.ReadToEnd();
+                richtext.Text = data;
+                sr.Close();
+
+
+                
+
+                filename.Text = Path.GetFileName(path);
+                directory.Text = path;
+            }
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+            SaveFileDialog sfd = new SaveFileDialog();
+            DialogResult dr = sfd.ShowDialog();
+            if(dr==DialogResult.OK)
+            {
+                string path = sfd.FileName;
+                File.WriteAllLines(path,richtext.Lines);
+            }
+           
+           
+        }
+
+       
+
+        private void compileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            Process pcom = new Process();
+            //  pcom.StartInfo.FileName = "javac";
+            pcom.StartInfo.FileName = "gcc.exe";
+            string path = "C:\\MinGW\\bin\\add.c";
+            pcom.StartInfo.WorkingDirectory = "C:\\MinGW\\bin";
+            pcom.StartInfo.Arguments = path ;   //filename.Text;
+
+            pcom.StartInfo.CreateNoWindow = true;
+            pcom.StartInfo.UseShellExecute = false;
+            pcom.StartInfo.RedirectStandardError = true;
+            pcom.StartInfo.RedirectStandardOutput = true;
+
+            pcom.Start();
+            pcom.WaitForExit();
+
+            string error = pcom.StandardError.ReadToEnd();
+            string output = pcom.StandardOutput.ReadToEnd();
+
+            if (error!="")
+            {
+                richcompiler.Text = error;
+                //MessageBox.Show("compile error");
+            }
+            else
+            {
+               richcompiler.Text = output;
+                richcompiler.Text = "Compilation done";
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void filename_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void runToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process pcom = new Process();
+            //  pcom.StartInfo.FileName = "javac";
+            pcom.StartInfo.FileName = "a.exe";
+           
+            pcom.StartInfo.WorkingDirectory = "C:\\MinGW\\bin";
+             //filename.Text;
+
+            pcom.StartInfo.CreateNoWindow = true;
+            pcom.StartInfo.UseShellExecute = false;
+            pcom.StartInfo.RedirectStandardError = true;
+            pcom.StartInfo.RedirectStandardOutput = true;
+
+            pcom.Start();
+            pcom.WaitForExit();
+
+            string error = pcom.StandardError.ReadToEnd();
+            string output = pcom.StandardOutput.ReadToEnd();
+
+            if (error != "")
+            {
+                richcompiler.Text = error;
+                //MessageBox.Show("compile error");
+            }
+            else
+            {
+                richcompiler.Text = output;
+
+            }
+        }
+
+        private void saveToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            string path1 = "C:\\MinGW\\bin\\add.c";
+            File.WriteAllLines(path1,richtext.Lines);
+        }
+
+        private void richtext_TextChanged(object sender, EventArgs e)
+        {
+            int c = richtext.Lines.Count();
+            lines.Text = c.ToString();
+
+        }
+    }
+}
